716e6b77430e6566fdfed47390f3c78088b4fa56
Adayair <adairolegario10agosto@gmail.com>
2024-08-02 16:08:43 -0700

    Update InputImage component with custom file upload styling ✅

--- src/components/InputImage.svelte
+++ src/components/InputImage.svelte
@@ -1,11 +1,57 @@
 <script>
-
 </script>
 
-<div class="input-image">
-  <input type="file" accept="image/*"/>
-</div>
+<label for="file" class="custum-file-upload">
+  <div class="icon">
+    <img src="/image.svg" alt="" />
+  </div>
+  <div class="text">
+    <span>Click to upload image</span>
+  </div>
+  <input id="file" type="file" accept="image/*" />
+</label>
 
 <style>
+  .custum-file-upload {
+    height: 200px;
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: space-between;
+    gap: 20px;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    border: 2px dashed #e8e8e8;
+    background-color: #212121;
+    padding: 1.5rem;
+    border-radius: 10px;
+    box-shadow: 0px 48px 35px -48px #e8e8e8;
+  }
 
+  .custum-file-upload .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .custum-file-upload .icon img {
+    height: 80px;
+    fill: #e8e8e8;
+  }
+
+  .custum-file-upload .text {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .custum-file-upload .text span {
+    font-weight: 400;
+    color: #e8e8e8;
+  }
+
+  .custum-file-upload input {
+    display: none;
+  }
 </style>
