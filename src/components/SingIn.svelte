6f89a3b72319a82825b3a0b2d072c9723f90bc05
Adayair <adairolegario10agosto@gmail.com>
2024-08-02 00:41:24 -0700

    feat: Add Astro configuration files and dependencies
    This commit adds the necessary configuration files and dependencies for Astro, a static site builder. It includes changes to the svelte.config.js, package.json, and astro.config.mjs files, as well as the addition of the @astrojs/svelte, svelte, and typescript dependencies. The purpose of this commit is to set up Astro for the project and enable its functionality. 🚀

---
+++ src/components/SingIn.svelte
@@ -0,0 +1,56 @@
+<script>
+  /*import { onMount } from "svelte";
+  import { navigate } from "svelte-routing";
+  import { auth } from "./stores/auth";
+
+  onMount(() => {
+    auth.subscribe((value) => {
+      if (value) {
+        navigate("/");
+      }
+    });
+  });*/
+
+  let showForm = false;
+
+  function togglePopover() {
+    showForm = !showForm;
+  }
+</script>
+
+<div class:show={showForm} class="form">
+  <form action="">
+    <input type="text" required placeholder="Email" />
+    <input type="password" required placeholder="Password" />
+    <button type="submit"> Sing In </button>
+  </form>
+</div>
+<button id="singIn" type="button" on:click={togglePopover}>Sing In</button>
+
+<style>
+  .form {
+    display: none;
+    border-radius: 15px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #a8a29e;
+    padding: 20px;
+    z-index: 1000;
+  }
+
+  .form {
+    &.show {
+      display: block;
+    }
+  }
+
+  #singIn {
+    border: none;
+    border-radius: 15px;
+    cursor: pointer;
+    font-weight: 900;
+    padding: 12px 15px;
+  }
+</style>
